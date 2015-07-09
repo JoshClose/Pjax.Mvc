e6778facaa1e64f653d8babe7ac933ab3975cecc
Josh Close <narshe@gmail.com>
2015-07-08 22:34:19 -0500

    Added example website.

---
+++ src/Example/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Pjax.Mvc5;
+
+namespace Example.Controllers
+{
+	[Pjax]
+    public class HomeController : Controller, IPjax
+    {
+		public bool IsPjaxRequest { get; set; }
+
+		public string PjaxVersion { get; set; }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+	    public ActionResult Page1()
+	    {
+		    return View();
+	    }
+
+		public ActionResult Page2()
+		{
+			return View();
+		}
+
+		public ActionResult Page3()
+		{
+			return View();
+		}
+    }
+}
